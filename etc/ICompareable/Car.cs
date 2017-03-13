d61a1ca9f3277c398b3a37d60e606237d80ef6f8
rizvi-kth <rizvi.kth@gmail.com>
2017-03-13 16:30:14 +0100

    Icomparable

---
+++ etc/ICompareable/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ICompareable
+{
+    internal class Car : IComparable<Car>
+    {
+        public Car()
+        {
+
+        }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+        public int CompareTo(Car other)
+        {
+            // If you want to sort by Name
+            return String.Compare(this.Name, other.Name, StringComparison.OrdinalIgnoreCase);
+
+            // If you want to sort by Price
+            //return this.Price.CompareTo(other.Price);
+        }
+
+        private class SortPriceAscendingComparer : IComparer<Car>
+        {
+            public int Compare(Car c1, Car c2)
+            {
+                if (c1.Price > c2.Price)
+                    return 1;
+
+                if (c1.Price < c2.Price)
+                    return -1;
+
+                else
+                    return 0;
+            }
+        }
+
+        // Method to return IComparer object for sort helper.
+        public static IComparer<Car> SortPriceAscending()
+        {
+            return (IComparer<Car>) new SortPriceAscendingComparer();
+        }
+
+        private class SortPriceDescendingComparer : IComparer<Car>
+        {
+            public int Compare(Car c1, Car c2)
+            {
+                if (c1.Price < c2.Price)
+                    return 1;
+                if (c1.Price > c2.Price)
+                    return -1;
+                else
+                    return 0;
+            }
+        }
+
+        // Method to return IComparer object for sort helper.
+        public static IComparer<Car> SortPriceDescending()
+        {
+            return (IComparer<Car>) new SortPriceDescendingComparer();
+        }
+
+    }
+}
---
+++ etc/ICompareable/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ICompareable
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Car car = new Car();
+        private List<Car> cars; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+
+            cars = new List<Car>()
+            {
+                new Car() { Name = "Crasler", Price = 300000.00},
+                new Car() { Name = "Marcidiz", Price = 350000.00},
+                new Car() { Name = "BMW", Price = 400000.00},
+                new Car() { Name = "McLaren", Price = 700000.00} 
+            };
+            listBox.ItemsSource = cars;
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.ItemsSource = null;
+            cars.Sort();
+            listBox1.ItemsSource = cars;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            //var arrayCars = cars.ToArray();
+            //Array.Sort(arrayCars, Car.SortPriceAscending());
+            //listBox1.ItemsSource = arrayCars;
+            listBox1.ItemsSource = null;
+            cars.Sort(Car.SortPriceAscending());
+            listBox1.ItemsSource = cars;
+
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            //var arrayCars = cars.ToArray();
+            //Array.Sort(arrayCars, Car.SortPriceDescending());
+            //listBox1.ItemsSource = arrayCars;
+            listBox1.ItemsSource = null;
+            cars.Sort(Car.SortPriceDescending());
+            listBox1.ItemsSource = cars;
+
+
+        }
+    }
+}
