c26e567cab28e2fdc97a26a40c3f535bf3a49e7f
rizvi-kth <rizvi.kth@gmail.com>
2017-03-16 22:20:22 +0100

    GC_Using added

---
+++ etc/GC_using.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    public static void Main()
+    {
+        
+        // This loop will continuously create a lot of unmanaged objects.
+        while (true)
+        {
+
+            // Using stement will automatically dispose the unmanaged objects. 
+            // So the memory usage will NOT rise by time in the while loop.
+            using (FileStream fs = File.Open(Path.Combine(@"C:\temp\testfile.txt"), FileMode.Open,FileAccess.Read,FileShare.Read))
+            {
+
+                // Without Using stement the unmanaged objects will NOT BE automatically disposed. 
+                // So the memory usage will rise by time in the while loop.
+                //FileStream fs = File.Open(Path.Combine(@"C:\temp\testfile.txt"), FileMode.Open, FileAccess.Read, FileShare.Read);
+
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    Console.WriteLine(temp.GetString(b).Trim());
+                }
+
+                // Every time a new object will be created with a new Hash-Code.
+                Console.WriteLine("Object Hashcode:{0}",fs.GetHashCode());
+            }
+        }
+
+    }
+}
