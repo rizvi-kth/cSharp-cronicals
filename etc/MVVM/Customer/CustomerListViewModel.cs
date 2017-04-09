7a2c9427702109cd92a417c3eb94564a1808e8bb
rizvi-kth <rizvi.kth@gmail.com>
2017-04-09 18:36:36 +0200

    mvvm template binding

---
+++ etc/MVVM/Customer/CustomerListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace MVVM.Customer
+{
+    public class CustomerListViewModel:BaseViewModel
+    {
+        public CustomerListViewModel()
+        {
+            CustomerList = new List<Customer>()
+            {
+                new Customer() {Name = "Robin",Adress = "Stockholm"},
+                new Customer() {Name = "Rifat",Adress = "New work"}
+            };        
+        }
+
+        public List<Customer> CustomerList { get; set; }
+        
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+
+    }
+}
---
+++ etc/MVVM/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.CodeDom;
+using System.Windows.Input;
+using MVVM.Customer;
+using MVVM.Order;
+using MVVM.View.ViewModel;
+
+namespace MVVM
+{
+    public class MainWindowViewModel :BaseViewModel
+    {
+        private BaseViewModel _currentViewModel;
+
+        public MainWindowViewModel()
+        {
+            CurrentViewModel = new CustomerListViewModel();
+            LoadCustomerView = new RelayCommand( (o) => { CurrentViewModel = new CustomerListViewModel(); }, (o) => true);
+            LoadOrderView = new RelayCommand((o) => { CurrentViewModel = new OrderListViewModel(); }, (o) => true);
+        }
+
+        public BaseViewModel CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            private set {
+                _currentViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand LoadCustomerView { get; private set; } 
+        public ICommand LoadOrderView { get; private set; }
+
+
+
+    }
+}
