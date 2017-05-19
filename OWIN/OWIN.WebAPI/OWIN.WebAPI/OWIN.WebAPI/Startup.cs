bf6a0e854f05e7496f188d3ebc530cc4a13b2e7a
rizvi-kth <rizvi.kth@gmail.com>
2017-05-19 22:34:18 +0200

    Little change

--- OWIN/OWIN.WebAPI/OWIN.WebAPI/OWIN.WebAPI/Startup.cs
+++ OWIN/OWIN.WebAPI/OWIN.WebAPI/OWIN.WebAPI/Startup.cs
@@ -8,8 +8,8 @@ using Owin;
 
 namespace OWIN.WebAPI
 {
-    // Install for Host   : Microsoft.Owin.Host.SystemWeb
-    // Install for WebAPI : Microsoft.AspNet.WebApi.Owin
+    // Install to add capability of runing in IIS pipeline : Microsoft.Owin.Host.SystemWeb
+    // Install for WebAPI                                  : Microsoft.AspNet.WebApi.Owin
     // Application_Start VS Startup.Configuration read:
     // http://stackoverflow.com/questions/20168978/do-i-need-a-global-asax-cs-file-at-all-if-im-using-an-owin-startup-cs-class-and
     public class Startup
