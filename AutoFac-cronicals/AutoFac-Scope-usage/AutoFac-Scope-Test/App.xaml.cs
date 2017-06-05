0b19876c313bdda344597b535185376813874c5a
tooextriha <tooextriha@TOOLSE002412>
2017-06-05 13:04:11 +0200

    test improvement

--- AutoFac-cronicals/AutoFac-Scope-usage/AutoFac-Scope-Test/App.xaml.cs
+++ AutoFac-cronicals/AutoFac-Scope-usage/AutoFac-Scope-Test/App.xaml.cs
@@ -40,15 +40,14 @@ namespace AutoFac_Scope
 
             Stopwatch watch = new Stopwatch();
             watch.Start();
-                                    
-            // Factory on Root Scope but takes Child Scope
-            var childScope = rootScope.BeginLifetimeScope();
-            Parameter[] param = new Parameter[] { new NamedParameter("scope", childScope) };
-            ViewModelFactory fac = rootScope.Resolve<ViewModelFactory>(param);
-
             // MainWindowViewModel on Root Scope
             MainWindowViewModel _mainWindowViewModel = rootScope.Resolve<MainWindowViewModel>();
 
+            // Factory on Child Scope and takes Child Scope to spawn ViewModels
+            var childScope = rootScope.BeginLifetimeScope();
+            Parameter[] param = new Parameter[] { new NamedParameter("scope", childScope) };
+            ViewModelFactory fac = childScope.Resolve<ViewModelFactory>(param);
+                        
             // WorldViewModel on Child Scope
             var WorldVM = fac.GetViewModelIns<WorldViewModel>();
             Debug.WriteLine($"WorldVM : {WorldVM.GetHashCode()}");
