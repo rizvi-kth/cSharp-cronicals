4913ee36610f3789368aa82886024e55e15a9d48
tooextriha <tooextriha@TOOLSE002412>
2017-07-13 10:38:20 +0200

    Fixed white

--- WPF/WhiteTestWPF/UnitTestProject1/UnitTest1.cs
+++ WPF/WhiteTestWPF/UnitTestProject1/UnitTest1.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Windows.Automation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestStack.White;
 using TestStack.White.UIItems.WindowItems;
 using TestStack.White.Factory;
 using TestStack.White.UIItems;
+using TestStack.White.UIItems.Custom;
+using TestStack.White.UIItems.Finders;
 
 namespace UnitTestProject1
 {
@@ -13,14 +16,19 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            Application application = Application.Launch(@"C:\Users\A547184\Documents\Visual Studio 2015\Projects\WhiteTestWPF\WhiteTestWPF\bin\Debug\WhiteTestWPF");
+            Application application = Application.Launch(@"C:\Users\TOOextriha\Documents\Git\cSharp-cronicals\WPF\WhiteTestWPF\WhiteTestWPF\bin\Debug\WhiteTestWPF.exe");
             Window window = application.GetWindow("MainWindow", InitializeOption.NoCache);
+
+            var uiItems = window.GetMultiple(SearchCriteria.ByControlType(ControlType.Custom));
+            var uiItems2 = window.Get<UserControl>(SearchCriteria.ByAutomationId("MyParent"));
+            uiItems2.DoTheJob();
+
 
             Button button = window.Get<Button>("button");
             button.Click();
 
-            Button button2 = window.Get<Button>("yellowButton");
-            button2.Click();
+            //Button button2 = window.Get<Button>("yellowButton");
+            //button2.Click();
 
 
         }
---
+++ WPF/WhiteTestWPF/UnitTestProject1/UserControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using TestStack.White.UIA;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Actions;
+using TestStack.White.UIItems.Custom;
+
+namespace UnitTestProject1
+{
+    [ControlTypeMapping(CustomUIItemType.Custom, WindowsFramework.Wpf)]
+    public class UserControl : CustomUIItem
+    {
+        public UserControl(
+            AutomationElement automationElement,
+            ActionListener actionListener)
+            : base(automationElement, actionListener)
+        {
+        }
+
+        protected UserControl()
+        {
+        }
+
+        public virtual void DoTheJob()
+        {
+            //Base class, i.e. CustomUIItem has property called Container. Use this find the items within this.
+            //Can also use SearchCriteria for find items
+            Button button2 = Container.Get<Button>("yellowButton");
+            button2.Click();
+
+            //Container.Get<TextBox>("day").Text = dateTime.Day.ToString();
+
+
+            //Container.Get<TextBox>("month").Text = dateTime.Month.ToString();
+            //Container.Get<TextBox>("year").Text = dateTime.Year.ToString();
+        }
+    }
+}
