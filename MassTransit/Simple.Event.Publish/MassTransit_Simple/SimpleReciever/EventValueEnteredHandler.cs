bda7ebada01116220e98966cd8140bd6fda672b0
tooextriha <tooextriha@TOOLSE002412>
2017-07-14 14:20:00 +0200

    MassTransit added

---
+++ MassTransit/Simple.Event.Publish/MassTransit_Simple/SimpleReciever/EventValueEnteredHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassTransit;
+using SimpleMessages;
+
+namespace SimpleReciever
+{
+    public class EventValueEnteredHandler :
+        IConsumer<EventValueEntered>
+    {
+        public async Task Consume(ConsumeContext<EventValueEntered> context)
+        {
+            await Console.Out.WriteLineAsync($"Updating customer: {context.Message.Value}");
+
+            // update the customer address
+        }
+    }
+}
---
+++ MassTransit/Simple.Event.Publish/MassTransit_Simple/SimpleReciever/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassTransit;
+
+
+namespace SimpleReciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://localhost/"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                cfg.ReceiveEndpoint(host, "tesla" , e =>
+                {
+                    e.Consumer<EventValueEnteredHandler>();
+                });
+            });
+
+            busControl.Start();
+            Console.WriteLine("Listining on tesla");
+            Console.ReadKey();
+            busControl.Stop();
+
+        }
+    }
+}
